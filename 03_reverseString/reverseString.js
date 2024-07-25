ec8d580baf4302f9547be3eddbe82758e12d13bf
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-07-25 20:02:45 +0200

    Exercise 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+let text = '';
+let rText = '';
+// console.log(text);
 
+const reverseString = function(text) {
+    // console.log(text.length);
+    rText ='';
+    for(i = 0 ; i <= text.length; i ++){
+        rText += text.charAt(text.length - i);
+        console.log(rText);
+    }
+    return rText;
 };
+console.log(reverseString(text));
 
 // Do not edit below this line
 module.exports = reverseString;
