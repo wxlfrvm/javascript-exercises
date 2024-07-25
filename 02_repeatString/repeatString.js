3db9a919044705e08171ab191df83961d444d9b1
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-07-25 19:26:14 +0200

    Exercises 01 and 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+num = 0;
+string = '';
 
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num < 0){
+        return newString = ("ERROR");
+
+    } else {
+        for(let i = 1 ; i <= num; i ++){
+            newString += string;
+        }
+        return newString;
+    }
 };
+// console.log(repeatString(string, num));
 
 // Do not edit below this line
 module.exports = repeatString;
