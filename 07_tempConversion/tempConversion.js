c9edb5d91bb0676d7963e29219e74f62e55b6c39
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-07-30 21:59:43 +0200

    js 07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  newTemp = (temp - 32) * (5 / 9);
+  newTemp = Math.round(newTemp*10) / 10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  newTemp = temp * (9 / 5) + 32;
+  newTemp = Math.round(newTemp*10) / 10;
+  return newTemp;
 };
+
+
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(73.2));
 
 // Do not edit below this line
 module.exports = {
