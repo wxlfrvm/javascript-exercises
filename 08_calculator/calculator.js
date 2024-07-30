1e42d6aaa5f89d1c95702db20ae9ae41c129f16f
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-07-30 22:17:38 +0200

    js 08_calculator: completed add, subtratc and sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,21 @@
-const add = function() {
-	
+const add = function(a, b) {
+  result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  console.log(array);
+
+	for(i = 0; i < array.length; i++ ){
+    result += array[i];
+  };
+  return result;
 };
 
 const multiply = function() {
@@ -21,6 +29,9 @@ const power = function() {
 const factorial = function() {
 	
 };
+
+console.log(add(2,4));
+console.log(sum([1,2,3,4]));
 
 // Do not edit below this line
 module.exports = {
