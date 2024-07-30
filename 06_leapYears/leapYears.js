a839060b043c76b6bd678c189aab2449f7143bd8
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-07-30 21:38:01 +0200

    js 06_leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    function divByFour(year){
+        if( year % 4 == 0 && year % 100 != 0 ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function divByFourHundred(year){ 
+        if( year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    if(divByFour(year) || divByFourHundred(year)){
+        isLeap = true;
+    } else {
+        isLeap = false;
+    }
+
+    return isLeap;
 };
 
 // Do not edit below this line
