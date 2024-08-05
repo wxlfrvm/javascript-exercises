efb9dadd2f02c706b74bbc9bcafc055ce1e3dc63
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-08-05 21:18:11 +0200

    js 10_fibonacci  exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    let a = 1;
+    let b = 1;
+    let result = 1;
 
+    if(nth < 0) return "OOPS";
+    if(nth == 0) return 0;
+    
+    for(i = 2 ; i < nth ; i++){   
+        result = a + b;
+        b = a;
+        a = result;
+        console.log(a, b, result);
+        } 
+    return result;
 };
 
 // Do not edit below this line
