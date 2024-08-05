2daf48dfa94e47b5aad5d742f7be5a3764154700
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-08-05 20:39:37 +0200

    js 09_palindromes exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let regexp = /[a-z0-9]/gi;
+    oldW = word.toLowerCase();
+    revW = '';
+    oldWR = oldW.match(regexp);
+    let newWR = '';
 
+    oldWR = oldWR.join("");
+    newWR = oldWR.toString();
+    
+    for(i = newWR.length; i >= 0 ; i--){
+        revW += newWR.charAt(i);
+    }
+
+    if (revW == newWR){
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
