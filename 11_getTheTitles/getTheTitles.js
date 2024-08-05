da73bb9007526ac2dcce441e3e8f5b3891523eba
wxlfrvm <172314920+wxlfrvm@users.noreply.github.com>
2024-08-05 22:19:48 +0200

    js 12_findTheOldest completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+let booksR = [];
 
-};
+const getTheTitles = function(input) {
+  for(i = 0 ; i < input.length ; i ++){
+  console.log(i, input[i].title);
+  booksR[i] = input[i].title;
+}
+  let result = booksR;
+  return result;
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
